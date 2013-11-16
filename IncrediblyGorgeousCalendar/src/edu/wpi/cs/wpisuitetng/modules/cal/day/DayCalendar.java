e24fdace18ba0175df7059ff40daa0ac532b973e
Ted Meyer <tmathmeyer@gmail.com>
2013-11-15 19:38:36 -0500

    added the skeletons of the other calendar types (month/day), might work on those once the other user stories are done. Also, we should add some sort of text file with user stories in it to the dev branch, so we can see how far along each are

---
+++ IncrediblyGorgeousCalendar/src/edu/wpi/cs/wpisuitetng/modules/cal/day/DayCalendar.java
@@ -0,0 +1,54 @@
+package edu.wpi.cs.wpisuitetng.modules.cal.day;
+
+import java.util.List;
+
+import org.joda.time.DateTime;
+
+import edu.wpi.cs.wpisuitetng.modules.cal.CalendarInterface;
+import edu.wpi.cs.wpisuitetng.modules.cal.models.Event;
+
+public class DayCalendar implements CalendarInterface {
+
+	@Override
+	public void next() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void previous() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void display(DateTime newTime) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addEvent(Event event) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addEvents(List<Event> eventList) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeEvent(Event event) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeEvents(List<Event> event) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ IncrediblyGorgeousCalendar/src/edu/wpi/cs/wpisuitetng/modules/cal/week/WeekCalendar.java
@@ -0,0 +1,58 @@
+package edu.wpi.cs.wpisuitetng.modules.cal.week;
+
+import java.util.List;
+
+import org.joda.time.DateTime;
+
+import edu.wpi.cs.wpisuitetng.modules.cal.CalendarInterface;
+import edu.wpi.cs.wpisuitetng.modules.cal.day.DayCalendar;
+import edu.wpi.cs.wpisuitetng.modules.cal.models.Event;
+
+public class WeekCalendar implements CalendarInterface {
+	
+	// does this make sense? if not it should be pretty easy to change, just a skeleton for now.
+	DayCalendar[] calendar = new DayCalendar[7];
+	
+	@Override
+	public void next() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void previous() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void display(DateTime newTime) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addEvent(Event event) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addEvents(List<Event> eventList) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeEvent(Event event) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeEvents(List<Event> event) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
