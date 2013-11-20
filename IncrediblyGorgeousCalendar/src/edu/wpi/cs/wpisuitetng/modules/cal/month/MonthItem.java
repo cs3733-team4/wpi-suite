7591a064f973e3aaa13a6ee206cebf0f6c8b3db6
Patrick Plenefisch <simonpatp@gmail.com>
2013-11-20 08:27:45 -0500

    Fixing 01 in MonthCal display

--- IncrediblyGorgeousCalendar/src/edu/wpi/cs/wpisuitetng/modules/cal/month/MonthItem.java
+++ IncrediblyGorgeousCalendar/src/edu/wpi/cs/wpisuitetng/modules/cal/month/MonthItem.java
@@ -63,7 +63,12 @@ public class MonthItem extends JPanel
 			ret = Integer.toString(when.getHourOfDay());
 
 		if (when.getMinuteOfHour() != 0)
-			ret += ":" + Integer.toString(when.getMinuteOfHour());
+		{
+			ret += ":";
+			if (when.getMinuteOfHour() < 10)
+				ret += "0";
+			ret += Integer.toString(when.getMinuteOfHour());
+		}
 
 		if (pm)
 			ret += "p";
