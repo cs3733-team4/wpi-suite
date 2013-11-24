cb762568e55e9edad3adf0fc565addb972b0047e
Anthony Joseph Ruffa <ajosephruffa@gmail.com>
2013-11-23 19:11:25 -0500

    Fixed the tests.

--- IncrediblyGorgeousCalendar/test/edu/wpi/cs/wpisuitetng/modules/cal/month/MonthItemTest.java
+++ IncrediblyGorgeousCalendar/test/edu/wpi/cs/wpisuitetng/modules/cal/month/MonthItemTest.java
@@ -23,33 +23,16 @@ public class MonthItemTest {
 	Displayable event = new Event();
 	
 	@Test
-	public void testGetWhen() {
-		MonthItem mItem = new MonthItem(event);
-		//MonthItem mItem = new MonthItem(time, "Test");
-		assertEquals("getWhen should return the DateTime provided", time, mItem.getWhen());
-	}
-	
-	@Test
-	public void testSetWhen() {
-		MonthItem mItem = new MonthItem(event);
-		//MonthItem mItem = new MonthItem(time, "Test");
-		mItem.setWhen(timeModified);
-		assertEquals("getWhen should return the modified DateTime by setWhen", timeModified, mItem.getWhen());
-	}
-	
-	@Test
 	public void testSimpleTimeAM() {
 		MonthItem mItem = new MonthItem(event);
-		//MonthItem mItem = new MonthItem(timeForSimpleTime, "Test");
-		assertEquals("simpleTime should return in hour:minute", "1:1", mItem.simpleTime(timeForSimpleTime));
+		assertEquals("simpleTime should return in hour:minute", "1:01", mItem.simpleTime(timeForSimpleTime));
 	}
 	
 	@Test
 	public void testSimpleTimePM() {
 		MonthItem mItem = new MonthItem(event);
-		//MonthItem mItem = new MonthItem(timeForSimpleTimePM, "Test");
 		assertEquals("simpleTime should return in hour:minute. If time is past 12:00, a p should be appended to the end",
-				"10:1p", mItem.simpleTime(timeForSimpleTimePM));
+				"10:01p", mItem.simpleTime(timeForSimpleTimePM));
 	}
 
 }
