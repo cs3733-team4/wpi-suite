8143d27d14d20c151542c4f3bb9867145d5e3ecb
escraire <escraire@gmail.com>
2013-11-25 21:56:22 -0500

    Added CategorySelector JPanel class

---
+++ IncrediblyGorgeousCalendar/src/edu/wpi/cs/wpisuitetng/modules/cal/eventui/CategoryManager.java
@@ -0,0 +1,11 @@
+package edu.wpi.cs.wpisuitetng.modules.cal.eventui;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JComboBox;
+
+public class CategoryManager extends JPanel {
+	public CategoryManager() {
+	}
+
+}
