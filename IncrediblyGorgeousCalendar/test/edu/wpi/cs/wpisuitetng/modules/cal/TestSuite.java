7af5de77db9f8c3c4bb9ae8634fbd869b69ba2f8
Zack Mason <zdmason@wpi.edu>
2013-12-02 22:28:22 -0500

    Updated TestSuite to account for new JUnit classes

--- IncrediblyGorgeousCalendar/test/edu/wpi/cs/wpisuitetng/modules/cal/TestSuite.java
+++ IncrediblyGorgeousCalendar/test/edu/wpi/cs/wpisuitetng/modules/cal/TestSuite.java
@@ -5,9 +5,13 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import edu.wpi.cs.wpisuitetng.modules.cal.ui.views.month.MonthCalendarTest;
+import edu.wpi.cs.wpisuitetng.modules.cal.models.EventEntityManagerTest;
+import edu.wpi.cs.wpisuitetng.modules.cal.models.CommitmentEntityManagerTest;
 import edu.wpi.cs.wpisuitetng.modules.cal.ui.views.month.MonthDayTest;
 import edu.wpi.cs.wpisuitetng.modules.cal.ui.views.month.MonthItemTest;
 import edu.wpi.cs.wpisuitetng.modules.cal.formulae.MonthsTest;
+import edu.wpi.cs.wpisuitetng.modules.cal.navigation.GoToPanelTest;
+import edu.wpi.cs.wpisuitetng.modules.cal.ui.EventUIValidationTest;
 import edu.wpi.cs.wpisuitetng.modules.cal.navigation.CalendarNavigationModuleTest;
 import edu.wpi.cs.wpisuitetng.modules.cal.year.YearCalendarTest;
 
@@ -19,7 +23,7 @@ import edu.wpi.cs.wpisuitetng.modules.cal.year.YearCalendarTest;
 
 
 @RunWith(Suite.class)
-@SuiteClasses({CalendarNavigationModuleTest.class, MonthItemTest.class, MonthCalendarTest.class, YearCalendarTest.class, MonthDayTest.class, MonthsTest.class})
+@SuiteClasses({CalendarNavigationModuleTest.class, EventEntityManagerTest.class, GoToPanelTest.class, EventUIValidationTest.class, CommitmentEntityManagerTest.class, MonthItemTest.class, MonthCalendarTest.class, YearCalendarTest.class, MonthDayTest.class, MonthsTest.class})
 
 public class TestSuite {
 
