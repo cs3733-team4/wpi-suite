4330327d5856e9ae5e834e514695df886ab966ec
Victor Andreoni <veandreonipaseka@wpi.edu>
2013-12-05 23:27:55 -0500

    Implemented final version of category indicator on month view.
    Implemented double arrows on left side of month item instead of one
    arrow on each side. Properly aligned arrows and commitment exclamation
    marks within category boxes. Cleaned up code, commented functions,
    rearranged code on file.

--- IncrediblyGorgeousCalendar/src/edu/wpi/cs/wpisuitetng/modules/cal/ui/views/month/MonthItem.java
+++ IncrediblyGorgeousCalendar/src/edu/wpi/cs/wpisuitetng/modules/cal/ui/views/month/MonthItem.java
@@ -12,13 +12,13 @@ package edu.wpi.cs.wpisuitetng.modules.cal.ui.views.month;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
 import org.joda.time.DateTime;
 
 import edu.wpi.cs.wpisuitetng.modules.cal.MainPanel;
@@ -33,112 +33,99 @@ import edu.wpi.cs.wpisuitetng.modules.cal.utils.Colors;
  */
 public class MonthItem extends JPanel
 {	
-	private JLabel time = new JLabel(), desc = new JLabel(), lastArrow = new JLabel();
-	
-	// TODO OPTION 2
+	private JLabel time = new JLabel(), name = new JLabel(), arrow = new JLabel("");
 	private JPanel categoryColor = new JPanel();
-			
 	private DateTime currentTime;
-	
 	private Displayable mDisplayable;
 	
-	
+	/**
+	 * Month Item constructor. When called without time, set time to current time
+	 * @param ndisp displayable to show on month item
+	 */
 	public MonthItem(Displayable ndisp)
 	{
 		this(ndisp, DateTime.now());
 	}
-	private boolean isStartBeforeCurrent(DateTime currentDay, DateTime eventStart)
-	{
-		if (eventStart.getYear()<currentDay.getYear())//if the year is less than its always true
-			return true;
-		else if (eventStart.getYear()>currentDay.getYear())
-			return false;
-		else if (eventStart.getDayOfYear()<currentDay.getDayOfYear())//year is the same, so only day matters
-			return true;
-		return false;
-	}
-	private boolean isEndAfterCurrent(DateTime currentDay, DateTime eventEnd)
-	{
-		if (eventEnd.getYear()>currentDay.getYear())//if the year is less than its always true
-			return true;
-		else if (eventEnd.getYear()<currentDay.getYear())
-			return false;
-		else if (eventEnd.getDayOfYear()>currentDay.getDayOfYear())//year is the same, so only day matters
-			return true;
-		return false;
-	}
+	
 	/**
 	 * MonthItem Constructor
-	 * @param when
-	 * @param descr
+	 * @param ndisp displayable to display
+	 * @param day time of month item
 	 */
 	public MonthItem(Displayable ndisp, DateTime day)
 	{
 		currentTime = day;
-		
+        this.mDisplayable = ndisp;
+        
+        // Set up Month Item Layout
         setBackground(Colors.TABLE_BACKGROUND);
         setMaximumSize(new java.awt.Dimension(32767, 24));
         setMinimumSize(new java.awt.Dimension(0, 0));
         setLayout(new javax.swing.BoxLayout(this, javax.swing.BoxLayout.X_AXIS));
 
-        this.mDisplayable = ndisp;
+        // Time label
+        time.setFont(new java.awt.Font("DejaVu Sans", Font.BOLD, 12));
+        time.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 3));
         
+        // Name label
+        name.putClientProperty("html.disable", true);
+        name.setText(mDisplayable.getName() + " ");
+        name.setFont(new java.awt.Font("DejaVu Sans", Font.PLAIN, 12));
+        name.setMinimumSize(new java.awt.Dimension(10, 15));
+        
+        // Arrow label (for multi-day events)
+        arrow.setFont(new java.awt.Font("DejaVu Sans", Font.BOLD, 12));
+        arrow.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 3));
+    	arrow.setAlignmentY(CENTER_ALIGNMENT);
+    	arrow.setAlignmentX(CENTER_ALIGNMENT);
+        
+        // Box for category color and arrow label
+        categoryColor.setPreferredSize(new Dimension(16, 15));
+    	categoryColor.setMaximumSize(new Dimension(16, 15));
+    	categoryColor.setMinimumSize(new Dimension(16, 15));
+    	categoryColor.setLayout(new GridLayout(1,1));
+        
+    	/** Display displayable based on whether it is an event or a commitment*/
+    	
+        // If displayable is commitment, show commitment sign and name
         if (ndisp instanceof Commitment)
         {
-
-            time.setFont(new java.awt.Font("DejaVu Sans", Font.BOLD, 14));
-        	time.setForeground(Colors.COMMITMENT_NOTIFICATION);
-        	time.setText("\u2762");
+        	arrow.setForeground(Colors.COMMITMENT_NOTIFICATION);
+        	arrow.setText("\uFF01");
+        	categoryColor.setBackground((Colors.TABLE_BACKGROUND));
+	    	categoryColor.setBorder(new EmptyBorder(0, 0, 0, 0));
         }
-        else if (ndisp instanceof Event)
+        else if (ndisp instanceof Event) // Else, show date, name, category, and whether event is multiple day
         {
-
-        	lastArrow.setFont(new java.awt.Font("DejaVu Sans", Font.BOLD, 12));
-            time.setFont(new java.awt.Font("DejaVu Sans", Font.BOLD, 12));
+        	categoryColor.setBackground(((Event) ndisp).getColor());
+        	categoryColor.setBorder(new EmptyBorder(0, 0, 0, 0));
+        	
         	if (isStartBeforeCurrent(day, ((Event)ndisp).getStart()) && isEndAfterCurrent(day, ((Event)ndisp).getEnd()))
         	{
-        		time.setText("\u2190");//the event goes before and after
-        		lastArrow.setText(" \u2192");
+        		arrow.setText("\u2194");//the event goes before and after
         	}
         	else if (isStartBeforeCurrent(day, ((Event)ndisp).getStart()))
         	{
-        		lastArrow.setText(null);
-        		time.setText("\u2190");
+        		arrow.setText("\u2190");
         	}
         	else if(isEndAfterCurrent(day, ((Event)ndisp).getEnd()))
         	{
         		time.setText(simpleTime(mDisplayable.getDate()));
-        		lastArrow.setText(" \u2192");
+        		arrow.setText("\u2192");
         	}
         	else
         	{
-        		lastArrow.setText(null);
         		time.setText(simpleTime(mDisplayable.getDate()));
         	}
         }
-        lastArrow.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 3));
-        time.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 3));
-        
-        // TODO OPTION 2
-        if (ndisp instanceof Event) {
-	        categoryColor.setPreferredSize(new Dimension(8, 14));
-	    	categoryColor.setMaximumSize(new Dimension(8, 14));
-	    	categoryColor.setBackground(((Event) ndisp).getColor());
-	    	categoryColor.setBorder(new EmptyBorder(0, 0, 0, 0));
-	    	add(categoryColor);
-        }
-        
+
+        // Add elements to UI
+        categoryColor.add(arrow);
+        add(categoryColor);
         add(time);
-        
-        desc.putClientProperty("html.disable", true);
-        desc.setText(mDisplayable.getName());
-        desc.setFont(new java.awt.Font("DejaVu Sans", Font.PLAIN, 12));
-        desc.setMinimumSize(new java.awt.Dimension(10, 15));
-        add(desc);
-        
-        if (lastArrow.getText()!=null)
-        	add(lastArrow);
-		
+        add(name);
+
+		// Set up click listener
 		addMouseListener(new MouseListener() {
 			
 			@Override
@@ -200,7 +187,13 @@ public class MonthItem extends JPanel
 		return ret;
 	}
 
-	
+	/**
+	 * Generate month item
+	 * @param elt displayable to display
+	 * @param selected selected displayable by Main Panel
+	 * @param day current date
+	 * @return month item
+	 */
 	public static Component generateFrom(Displayable elt, Displayable selected, DateTime day)
 	{
 		MonthItem mi = new MonthItem(elt, day);
@@ -216,18 +209,51 @@ public class MonthItem extends JPanel
 		return this.mDisplayable;
 	}
 	
+	/**
+	 * Sets selected month item in month view
+	 * @param elt displayable to check
+	 * @param item selected displayable
+	 */
 	public void setSelected(Displayable elt, Displayable item){
-		if(elt == item){
-			// Change background color
+		if(elt == item)
 			this.setBackground(Colors.SELECTED_BACKGROUND);
-		}
-		// TODO  OPTION 1
-		//else if (elt instanceof Event) 
-			//this.setBackground(((Event) elt).getColor());
 		else
-			// Change background color
 			this.setBackground(Colors.TABLE_BACKGROUND);
-
+	}
+	
+	
+	/**
+	 * Check if start date of event is before the specified day
+	 * @param currentDay day to compare event time with
+	 * @param eventStart event time
+	 * @return boolean determining whether the event start date is before current day
+	 */
+	private boolean isStartBeforeCurrent(DateTime currentDay, DateTime eventStart)
+	{
+		if (eventStart.getYear()<currentDay.getYear())//if the year is less than its always true
+			return true;
+		else if (eventStart.getYear()>currentDay.getYear())
+			return false;
+		else if (eventStart.getDayOfYear()<currentDay.getDayOfYear())//year is the same, so only day matters
+			return true;
+		return false;
+	}
+	
+	/**
+	 * Check if end date of event is after the specified day
+	 * @param currentDay day to compare event time with
+	 * @param eventEnd event time
+	 * @return boolean determining whether the event end date is after current day
+	 */
+	private boolean isEndAfterCurrent(DateTime currentDay, DateTime eventEnd)
+	{
+		if (eventEnd.getYear()>currentDay.getYear())//if the year is less than its always true
+			return true;
+		else if (eventEnd.getYear()<currentDay.getYear())
+			return false;
+		else if (eventEnd.getDayOfYear()>currentDay.getDayOfYear())//year is the same, so only day matters
+			return true;
+		return false;
 	}
 }
 
