8a1bc815f5a022ac898b6d0c5a8d5bd6a94b47aa
Brian Gallenstein <bgallenstein@gmail.com>
2013-12-07 18:41:59 -0500

    Set up the layout of week view with a grid of days

--- IncrediblyGorgeousCalendar/src/edu/wpi/cs/wpisuitetng/modules/cal/views/week/WeekCalendar.java
+++ IncrediblyGorgeousCalendar/src/edu/wpi/cs/wpisuitetng/modules/cal/views/week/WeekCalendar.java
@@ -10,8 +10,12 @@
 package edu.wpi.cs.wpisuitetng.modules.cal.views.week;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.util.List;
 
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -38,8 +42,14 @@ public class WeekCalendar extends AbstractCalendar
 	private MainPanel mainPanel;
 	private LouvreTour sun, mon, tue, wed, thu, fri, sat;
 	
-	private JPanel holder = new JPanel();
-	private JScrollPane scroll = new JScrollPane(holder);
+	private JPanel dayHolderPanel = new JPanel();
+	private JPanel dayGrid = new JPanel();
+	private JPanel dayGridContainer = new JPanel();
+	
+	private JPanel dayTitleGrid = new JPanel();
+	private JPanel dayTitlesContainer = new JPanel();
+	
+	private JScrollPane scroll = new JScrollPane(dayGridContainer);
 
 	private EventModel eventModel;
 
@@ -54,52 +64,94 @@ public class WeekCalendar extends AbstractCalendar
 		scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		scroll.getVerticalScrollBar().setUnitIncrement(20);
-		holder.setBackground(Colors.TABLE_BACKGROUND);
+		dayGrid.setBackground(Colors.TABLE_BACKGROUND);
 		
-		this.setLayout(new BorderLayout());
-		this.holder.setLayout(new BorderLayout());
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		this.dayHolderPanel.setLayout(new BorderLayout());
+		this.dayGrid.setLayout(new GridLayout(1, 7));
+		this.dayTitleGrid.setLayout(new GridLayout(1, 7));
+		this.dayTitlesContainer.setLayout(new BoxLayout(dayTitlesContainer, BoxLayout.X_AXIS));
+		this.dayGridContainer.setLayout(new BorderLayout()); //Nested BorderLayouts arent the best
 		
 		generateDay();
 	}
 
 	private void generateDay()
 	{
-		this.holder.removeAll();
+		this.dayGrid.removeAll();
 		this.removeAll();
-		this.add(scroll, BorderLayout.CENTER);
-		this.add(new JLabel(time.toString(titleFmt)), BorderLayout.NORTH);
-
+		
 		MutableDateTime increment=new MutableDateTime(time);
 		
 		this.sun = new LouvreTour();
 		this.sun.setEvents(getVisibleEvents(increment.toDateTime()), increment.toDateTime());
+		this.sun.setBorder(BorderFactory.createLineBorder(Colors.BORDER));
 		increment.addDays(1);
 		this.mon = new LouvreTour();
 		this.mon.setEvents(getVisibleEvents(increment.toDateTime()), increment.toDateTime());
+		this.mon.setBorder(BorderFactory.createLineBorder(Colors.BORDER));
 		increment.addDays(1);
 		this.tue = new LouvreTour();
 		this.tue.setEvents(getVisibleEvents(increment.toDateTime()), increment.toDateTime());
+		this.tue.setBorder(BorderFactory.createLineBorder(Colors.BORDER));
 		increment.addDays(1);
 		this.wed = new LouvreTour();
 		this.wed.setEvents(getVisibleEvents(increment.toDateTime()), increment.toDateTime());
+		this.wed.setBorder(BorderFactory.createLineBorder(Colors.BORDER));
 		increment.addDays(1);
 		this.thu = new LouvreTour();
 		this.thu.setEvents(getVisibleEvents(increment.toDateTime()), increment.toDateTime());
+		this.thu.setBorder(BorderFactory.createLineBorder(Colors.BORDER));
 		increment.addDays(1);
 		this.fri = new LouvreTour();
 		this.fri.setEvents(getVisibleEvents(increment.toDateTime()), increment.toDateTime());
+		this.fri.setBorder(BorderFactory.createLineBorder(Colors.BORDER));
 		increment.addDays(1);
 		this.sat = new LouvreTour();
 		this.sat.setEvents(getVisibleEvents(increment.toDateTime()), increment.toDateTime());
+		this.sat.setBorder(BorderFactory.createLineBorder(Colors.BORDER));
 
-		this.holder.add(DayGridLabel.getInstance(), BorderLayout.WEST);
-		this.holder.add(this.sun, BorderLayout.CENTER);
-		this.holder.add(this.mon, BorderLayout.EAST);
-		this.holder.add(this.tue, BorderLayout.EAST);
-		this.holder.add(this.wed, BorderLayout.EAST);
-		this.holder.add(this.thu, BorderLayout.EAST);
-		this.holder.add(this.fri, BorderLayout.EAST);
-		this.holder.add(this.sat, BorderLayout.EAST);
+		//add days to grid
+		this.dayGrid.add(this.sun);
+		this.dayGrid.add(this.mon);
+		this.dayGrid.add(this.tue);
+		this.dayGrid.add(this.wed);
+		this.dayGrid.add(this.thu);
+		this.dayGrid.add(this.fri);
+		this.dayGrid.add(this.sat);
+		
+		//add titles to grid
+		this.dayTitleGrid.removeAll();
+		this.dayTitleGrid.add(new JLabel("Sun", JLabel.CENTER));
+		this.dayTitleGrid.add(new JLabel("Mon", JLabel.CENTER));
+		this.dayTitleGrid.add(new JLabel("Tue", JLabel.CENTER));
+		this.dayTitleGrid.add(new JLabel("Wed", JLabel.CENTER));
+		this.dayTitleGrid.add(new JLabel("Thu", JLabel.CENTER));
+		this.dayTitleGrid.add(new JLabel("Fri", JLabel.CENTER));
+		this.dayTitleGrid.add(new JLabel("Sat", JLabel.CENTER));
+		
+		//initialize spacer label for dayGridLabel
+		JLabel spacerLabel = new JLabel("10:00    ");
+		spacerLabel.setForeground(spacerLabel.getBackground());
+		spacerLabel.setBorder(BorderFactory.createLineBorder(spacerLabel.getBackground()));
+		
+		//add grids to box container
+		this.dayTitlesContainer.removeAll();
+		this.dayTitlesContainer.add(spacerLabel);
+		this.dayTitlesContainer.add(dayTitleGrid);		
+
+		//add spacer and time labels to container
+		this.dayGridContainer.add(DayGridLabel.getInstance(), BorderLayout.WEST);
+		this.dayGridContainer.add(dayGrid, BorderLayout.CENTER);
+		
+		//add the sidebar and the day grid to the container panel
+		this.dayHolderPanel.add(dayTitlesContainer, BorderLayout.NORTH);
+		this.dayHolderPanel.add(scroll, BorderLayout.CENTER);
+		
+		//add title and week view to this
+		this.add(new JLabel(time.toString(titleFmt)));
+		this.add(dayHolderPanel);
+		
 		// notify mini-calendar to change
 		mainPanel.miniMove(time);
 	}
