ae00bd91ae158fd7bf0adae94540249a05daeda9
Patrick Plenefisch <simonpatp@gmail.com>
2013-12-08 01:30:19 -0500

    Adding lic header to stuff I touched

--- IncrediblyGorgeousCalendar/test/edu/wpi/cs/wpisuitetng/modules/cal/MockNetwork.java
+++ IncrediblyGorgeousCalendar/test/edu/wpi/cs/wpisuitetng/modules/cal/MockNetwork.java
@@ -5,7 +5,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
- * Contributors: Team Rolling Thunder
+ * Contributors: Team YOCO (You Only Compile Once)
  ******************************************************************************/
 package edu.wpi.cs.wpisuitetng.modules.cal;
 
