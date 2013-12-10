102523619e0dd2865eead4fef0c980d4979b8d3a
bkmcleod <bkmcleod@wpi.edu>
2013-12-09 19:40:16 -0500

    Added catch for un-implemented actions

--- IncrediblyGorgeousCalendar/src/edu/wpi/cs/wpisuitetng/modules/cal/documentation/DocumentMainPanel.java
+++ IncrediblyGorgeousCalendar/src/edu/wpi/cs/wpisuitetng/modules/cal/documentation/DocumentMainPanel.java
@@ -118,6 +118,11 @@ public class DocumentMainPanel extends JFrame{
     		ncm.setTabId(MainPanel.getInstance().addTopLevelTab(ncm, "New Commitment", true));
     		return true;
     	}
+    	if (actionPath.contains("#"))
+    	{
+    		System.out.println("Action: " + actionPath + " not yet implemented!");
+    		return true;
+    	}
     	return false;
     }
     
