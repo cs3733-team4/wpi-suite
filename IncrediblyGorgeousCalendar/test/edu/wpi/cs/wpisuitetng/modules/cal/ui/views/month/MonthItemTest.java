52156c2c5ea8a48c82839de5016c77ad0e774559
Ted Meyer <tmathmeyer@gmail.com>
2013-12-10 13:17:23 -0500

    added headers and removed some unused imports

--- IncrediblyGorgeousCalendar/test/edu/wpi/cs/wpisuitetng/modules/cal/ui/views/month/MonthItemTest.java
+++ IncrediblyGorgeousCalendar/test/edu/wpi/cs/wpisuitetng/modules/cal/ui/views/month/MonthItemTest.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright (c) 2013 WPI-Suite
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Team YOCO (You Only Compile Once)
+ ******************************************************************************/
 package edu.wpi.cs.wpisuitetng.modules.cal.ui.views.month;
 
 import static org.junit.Assert.*;
@@ -6,10 +15,6 @@ import edu.wpi.cs.wpisuitetng.modules.cal.ui.views.month.MonthItem;
 
 import org.joda.time.DateTime;
 import org.junit.Test;
-
-import edu.wpi.cs.wpisuitetng.modules.cal.models.Displayable;
-import edu.wpi.cs.wpisuitetng.modules.cal.models.Event;
-import edu.wpi.cs.wpisuitetng.modules.cal.ui.views.month.MonthItem;
 
 /**
  * Tests for MonthItem class
