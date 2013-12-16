f91d87bef9a40bae2b79b1baffca98eefd62049a
Ted Meyer <tmathmeyer@gmail.com>
2013-12-16 13:52:04 -0500

    added syncer threadable item

---
+++ Calendar/src/edu/wpi/cs/wpisuitetng/modules/cal/models/google/DisplayableSyncer.java
@@ -0,0 +1,33 @@
+package edu.wpi.cs.wpisuitetng.modules.cal.models.google;
+
+import java.io.IOException;
+
+import com.google.gdata.util.ServiceException;
+
+import edu.wpi.cs.wpisuitetng.modules.cal.models.Displayable;
+
+public class DisplayableSyncer implements Runnable {
+
+	private GoogleSync gs;
+	private Displayable d;
+	
+	public DisplayableSyncer(GoogleSync gs, Displayable d)
+	{
+		this.d = d;
+		this.gs = gs;
+		
+		new Thread(this).start();
+	}
+	
+	@Override
+	public void run() {
+		try {
+			gs.addEventToCalendar(d);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ServiceException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
